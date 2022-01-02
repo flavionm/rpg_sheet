83a930bfcdcd6a7aea5790b9097b680e5518ac71
Flávio Sousa <fn_maia@outlook.com>
2022-01-02 13:15:43 -0300

    Add character list

--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -10,6 +10,44 @@ class Home extends StatelessWidget {
       appBar: AppBar(
         title: Text(AppLocalizations.of(context)!.home_title),
       ),
+      body: charList.isNotEmpty
+          ? ListView.builder(
+              itemCount: charList.length,
+              itemBuilder: (context, index) {
+                return CharacterListItem(charList[index]);
+              },
+            )
+          : const Center(
+              child: Text('Nothing to see here'),
+            ),
     );
   }
+}
+
+class CharacterListItem extends StatelessWidget {
+  final _Character character;
+
+  const CharacterListItem(this.character, {Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      title: Text(character.name),
+      subtitle: Text('Level ${character.level} ${character.cClass}'),
+    );
+  }
+}
+
+const charList = <_Character>[
+  _Character('John', 5, 'Warrior'),
+  _Character('Mary', 4, 'Wizard'),
+  _Character('Jack', 6, 'Barbarian'),
+];
+
+class _Character {
+  final String name;
+  final int level;
+  final String cClass;
+
+  const _Character(this.name, this.level, this.cClass);
 }
